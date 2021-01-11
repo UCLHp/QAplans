af0d70f7c7b7404d0c7415e1b3b459b0de628a94
agosling <andrew.gosling@nhs.net>
2021-01-11 17:53:00 +0000

    updated old version to working

---
+++ renew/__init__.py
@@ -0,0 +1,10 @@
+from QAplans._pbtDICOM import PLANdata
+from QAplans._pbtDICOM import BEAMdata
+from QAplans._pbtDICOM import SPOTdata
+
+
+from QAplans._qaType import qaPlanType
+
+from QAplans._spotGenerator import spotGenerator
+
+from QAplans._writeDICOMplan import writeDICOMplan
