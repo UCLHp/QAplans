86cce3382b30df16c95a5d6ce0db5d4ed2cc8590
agosling <andrew.gosling@nhs.net>
2021-09-24 15:42:27 +0100

    should also now work on multiple files

--- _change_gantry.py
+++ _change_gantry.py
@@ -6,26 +6,31 @@ def change_gantry():
     '''Function to change the Treatment room tag for all beams in a dicom plan
     '''
     # Select file to be changed
-    my_path = eg.fileopenbox('Select plan file to be changed')
-    my_dcm = pydicom.dcmread(my_path)
+    my_path = eg.fileopenbox('Select plan file(s) to be changed', multiple=True)
+    if not isinstance(my_path, list):
+        list(my_path)
 
-    gantry = eg.choicebox('Room selection',
-                          'Select desired Gantry',
-                          ['Gantry 1', 'Gantry 2', 'Gantry 3', 'Gantry 4']
-                          )
-    print(f'Number of fields: {len(my_dcm.IonBeamSequence)}\n')
+        gantry = eg.choicebox('Room selection',
+                              'Select desired Gantry',
+                              ['Gantry 1', 'Gantry 2', 'Gantry 3', 'Gantry 4']
+                              )
 
-    count = 1
-    for field in my_dcm.IonBeamSequence:
-        print(f'Field {count} is currently {field.TreatmentMachineName}')
-        field.TreatmentMachineName = gantry
-        print(f'Field {count} changed to {field.TreatmentMachineName}\n')
+     for fl in my_path:
+        my_dcm = pydicom.dcmread(fl)
 
-    print('All fields changed\n')
+        # print(f'Number of fields: {len(my_dcm.IonBeamSequence)}\n')
 
-    print('Overwriting File\n')
-    my_dcm.save_as(my_path)
-    print('File Saved')
+        count = 1
+        for field in my_dcm.IonBeamSequence:
+            # print(f'Field {count} is currently {field.TreatmentMachineName}')
+            field.TreatmentMachineName = gantry
+            # print(f'Field {count} changed to {field.TreatmentMachineName}\n')
+
+        # print('All fields changed\n')
+
+        # print('Overwriting File\n')
+        my_dcm.save_as(fl)
+        # print('File Saved')
 
 
 if __name__ == '__main__':
